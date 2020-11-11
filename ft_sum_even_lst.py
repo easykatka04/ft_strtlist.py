d88c09610cef10f37c7aacd2f81fcf2694a07bec
easykatka04 <71816857+easykatka04@users.noreply.github.com>
2020-11-12 00:53:35 +0300

    Update ft_sum_even_lst.py

--- ft_sum_even_lst.py
+++ ft_sum_even_lst.py
@@ -1,1 +1,13 @@
+def ft_len(s):
+    l = 0
+    for i in s:
+        l += 1
+    return l
 
+
+def ft_sum_even_lst(a):
+    b = 0
+    for i in range(0, ft_len(a) + 1, 2):
+        b += i
+    return b
+  
