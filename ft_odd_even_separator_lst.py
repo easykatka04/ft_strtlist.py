a5958026d6da5de4f488e2da4423fe54fcb85ba6
easykatka04 <71816857+easykatka04@users.noreply.github.com>
2020-11-12 10:04:15 +0300

    Update ft_odd_even_separator_lst.py

--- ft_odd_even_separator_lst.py
+++ ft_odd_even_separator_lst.py
@@ -1,1 +1,12 @@
-
+def ft_odd_even_separator_lst(a):
+    h = []
+    neh = []
+    for i in a:
+        if i % 2 == 0:
+            h.append(i)
+        else:
+            neh.append(i)
+    a = []
+    a.append(h)
+    a.append(neh)
+    return a
