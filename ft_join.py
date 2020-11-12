682b052a03cb7da4a2185baefbca0adfaa635e44
easykatka04 <71816857+easykatka04@users.noreply.github.com>
2020-11-12 12:32:47 +0300

    Update ft_join.py

--- ft_join.py
+++ ft_join.py
@@ -1,7 +1,16 @@
+def ft_len(a):
+    b = 0
+    for i in a:
+        b += 1
+    return b
+
+
 def ft_join(a, b):
     c = ""
-    for i in a:
-        c += str(i)
-        c += b
+    d = 0
+    if b == "":
+        b = " "
+    while d != ft_len(a):
+        c += str(a[d]) + b
+        d += 1
     return c
-  
