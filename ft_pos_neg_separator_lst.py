7ba55ee1bdcc65ee40c09571fea553b3c934de57
easykatka04 <71816857+easykatka04@users.noreply.github.com>
2020-11-12 10:11:35 +0300

    Update ft_pos_neg_separator_lst.py

--- ft_pos_neg_separator_lst.py
+++ ft_pos_neg_separator_lst.py
@@ -1,1 +1,16 @@
-
+def ft_pos_neg_separator_lst(a):
+    otr = []
+    nool = []
+    pol = []
+    for i in a:
+        if i > 0:
+            pol.append(i)
+        if i < 0:
+            otr.append(i)
+        if i == 0:
+            nool.append(i)
+    a = []
+    a.append(otr)
+    a.append(nool)
+    a.append(pol)
+    return a
